5436cd2327b2970c41003d33adcd5ceab2a45ea9
Gabriel Lumertz <glumertz10@gmail.com>
2022-08-14 22:58:39 -0300

    incluindo todas as loterias

--- main.js
+++ main.js
@@ -11,9 +11,11 @@ const lotterys = () => {
 
             const list = document.getElementById("loterias")
 
-            const listItem = `<li class="${item}" onclick="${item.replace("-", "")}()">${item}</li>`
+            const listItem = `<li class="${item}" onclick="${item.replace("-", "").replace("-", "")}()">${item}</li>`
 
             list.innerHTML += listItem
+
+            console.log(item)
 
         });
         
@@ -24,20 +26,17 @@ lotterys()
 
 function megasena() {
 
+    function clearUl() {
+        let ulDezenas = document.getElementById("dezenas");
+        ulDezenas.innerText = ""
+    }
+    
+    clearUl()
+
     const url = `https://loteriascaixa-api.herokuapp.com/api/mega-sena/latest`
     fetch(url)
     .then(response => response.json())
     .then(data => {
-
-        result = []
-
-        result.push(data.dezenas)
-
-        console.log(result)
-
-        result.forEach((item) => {
-            
-        })
 
         const concurso = document.getElementById('concurso')
 
@@ -51,60 +50,363 @@ function megasena() {
 
         concurso.innerHTML = title
 
-        const dezena1 = document.getElementById('dezena1')
-        const dezena2 = document.getElementById('dezena2')
-        const dezena3 = document.getElementById('dezena3')
-        const dezena4 = document.getElementById('dezena4')
-        const dezena5 = document.getElementById('dezena5')
-        const dezena6 = document.getElementById('dezena6')
-        
-        dezena1.innerText = data.dezenas[0]
-        dezena2.innerText = data.dezenas[1]
-        dezena3.innerText = data.dezenas[2]
-        dezena4.innerText = data.dezenas[3]
-        dezena5.innerText = data.dezenas[4]
-        dezena6.innerText = data.dezenas[5]
+        result = []
 
+        result = data.dezenas
+
+        result.forEach((item) => {   
+
+            const list = document.getElementById("dezenas")
+
+            const listItem = `<li class="dezena">${item}</li>`
+
+            list.innerHTML += listItem
+
+        })
     })
     
 } 
 
-// const searchData = () => {
+function lotofacil() {
 
-//     const url = `https://loteriascaixa-api.herokuapp.com/api/mega-sena/latest`
-//     fetch(url)
-//     .then(response => response.json())
-//     .then(data => {
-
-//         const concurso = document.getElementById('concurso')
-
-//         const lottery = data.nome
-        
-//         const sorteio = data.concurso
-        
-//         const date = data.data
-
-//         const title = `Resultado da ${lottery} concurso ${sorteio} (${date})`
-
-//         concurso.innerHTML = title
-
-//         const dezena1 = document.getElementById('dezena1')
-//         const dezena2 = document.getElementById('dezena2')
-//         const dezena3 = document.getElementById('dezena3')
-//         const dezena4 = document.getElementById('dezena4')
-//         const dezena5 = document.getElementById('dezena5')
-//         const dezena6 = document.getElementById('dezena6')
-        
-//         dezena1.innerText = data.dezenas[0]
-//         dezena2.innerText = data.dezenas[1]
-//         dezena3.innerText = data.dezenas[2]
-//         dezena4.innerText = data.dezenas[3]
-//         dezena5.innerText = data.dezenas[4]
-//         dezena6.innerText = data.dezenas[5]
-
-//     })
-// }
-
-// searchData()
-
+    function clearUl() {
+        let ulDezenas = document.getElementById("dezenas");
+        ulDezenas.innerText = ""
+    }
     
+    clearUl()
+
+    const url = `https://loteriascaixa-api.herokuapp.com/api/lotofacil/latest`
+    fetch(url)
+    .then(response => response.json())
+    .then(data => {
+
+        const concurso = document.getElementById('concurso')
+
+        const lottery = data.nome
+        
+        const sorteio = data.concurso
+        
+        const date = data.data
+
+        const title = `Resultado da ${lottery} concurso ${sorteio} (${date})`
+
+        concurso.innerHTML = title
+
+        result = []
+
+        result = data.dezenas
+
+        result.forEach((item) => {   
+
+            const list = document.getElementById("dezenas")
+
+            const listItem = `<li class="dezena">${item}</li>`
+
+            list.innerHTML += listItem
+
+        })
+    })
+    
+}
+
+function quina() {
+
+    function clearUl() {
+        let ulDezenas = document.getElementById("dezenas");
+        ulDezenas.innerText = ""
+    }
+    
+    clearUl()
+
+    const url = `https://loteriascaixa-api.herokuapp.com/api/quina/latest`
+    fetch(url)
+    .then(response => response.json())
+    .then(data => {
+
+        const concurso = document.getElementById('concurso')
+
+        const lottery = data.nome
+        
+        const sorteio = data.concurso
+        
+        const date = data.data
+
+        const title = `Resultado da ${lottery} concurso ${sorteio} (${date})`
+
+        concurso.innerHTML = title
+
+        result = []
+
+        result = data.dezenas
+
+        result.forEach((item) => {   
+
+            const list = document.getElementById("dezenas")
+
+            const listItem = `<li class="dezena">${item}</li>`
+
+            list.innerHTML += listItem
+
+        })
+    })
+    
+}
+
+function lotomania() {
+
+    function clearUl() {
+        let ulDezenas = document.getElementById("dezenas");
+        ulDezenas.innerText = ""
+    }
+    
+    clearUl()
+
+    const url = `https://loteriascaixa-api.herokuapp.com/api/lotomania/latest`
+    fetch(url)
+    .then(response => response.json())
+    .then(data => {
+
+        const concurso = document.getElementById('concurso')
+
+        const lottery = data.nome
+        
+        const sorteio = data.concurso
+        
+        const date = data.data
+
+        const title = `Resultado da ${lottery} concurso ${sorteio} (${date})`
+
+        concurso.innerHTML = title
+
+        result = []
+
+        result = data.dezenas
+
+        result.forEach((item) => {   
+
+            const list = document.getElementById("dezenas")
+
+            const listItem = `<li class="dezena">${item}</li>`
+
+            list.innerHTML += listItem
+
+        })
+    })
+    
+}
+
+function timemania() {
+
+    function clearUl() {
+        let ulDezenas = document.getElementById("dezenas");
+        ulDezenas.innerText = ""
+    }
+    
+    clearUl()
+
+    const url = `https://loteriascaixa-api.herokuapp.com/api/timemania/latest`
+    fetch(url)
+    .then(response => response.json())
+    .then(data => {
+
+        const concurso = document.getElementById('concurso')
+
+        const lottery = data.nome
+        
+        const sorteio = data.concurso
+        
+        const date = data.data
+
+        const title = `Resultado da ${lottery} concurso ${sorteio} (${date})`
+
+        concurso.innerHTML = title
+
+        result = []
+
+        result = data.dezenas
+
+        result.forEach((item) => {   
+
+            const list = document.getElementById("dezenas")
+
+            const listItem = `<li class="dezena">${item}</li>`
+
+            list.innerHTML += listItem
+
+        })
+    })
+    
+}
+
+function duplasena() {
+
+    function clearUl() {
+        let ulDezenas = document.getElementById("dezenas");
+        ulDezenas.innerText = ""
+    }
+    
+    clearUl()
+
+    const url = `https://loteriascaixa-api.herokuapp.com/api/dupla-sena/latest`
+    fetch(url)
+    .then(response => response.json())
+    .then(data => {
+
+        const concurso = document.getElementById('concurso')
+
+        const lottery = data.nome
+        
+        const sorteio = data.concurso
+        
+        const date = data.data
+
+        const title = `Resultado da ${lottery} concurso ${sorteio} (${date})`
+
+        concurso.innerHTML = title
+
+        result = []
+
+        result = data.dezenas
+
+        result.forEach((item) => {   
+
+            const list = document.getElementById("dezenas")
+
+            const listItem = `<li class="dezena">${item}</li>`
+
+            list.innerHTML += listItem
+
+        })
+    })
+    
+}
+
+function loteriafederal() {
+
+    function clearUl() {
+        let ulDezenas = document.getElementById("dezenas");
+        ulDezenas.innerText = ""
+    }
+    
+    clearUl()
+
+    const url = `https://loteriascaixa-api.herokuapp.com/api/loteria-federal/latest`
+    fetch(url)
+    .then(response => response.json())
+    .then(data => {
+
+        const concurso = document.getElementById('concurso')
+
+        const lottery = data.nome
+        
+        const sorteio = data.concurso
+        
+        const date = data.data
+
+        const title = `Resultado da ${lottery} concurso ${sorteio} (${date})`
+
+        concurso.innerHTML = title
+
+        result = []
+
+        result = data.dezenas
+
+        result.forEach((item) => {   
+
+            const list = document.getElementById("dezenas")
+
+            const listItem = `<li class="dezena">${item}</li>`
+
+            list.innerHTML += listItem
+
+        })
+    })
+    
+}
+
+function diadesorte() {
+
+    function clearUl() {
+        let ulDezenas = document.getElementById("dezenas");
+        ulDezenas.innerText = ""
+    }
+    
+    clearUl()
+
+    const url = `https://loteriascaixa-api.herokuapp.com/api/dia-de-sorte/latest`
+    fetch(url)
+    .then(response => response.json())
+    .then(data => {
+
+        const concurso = document.getElementById('concurso')
+
+        const lottery = data.nome
+        
+        const sorteio = data.concurso
+        
+        const date = data.data
+
+        const title = `Resultado da ${lottery} concurso ${sorteio} (${date})`
+
+        concurso.innerHTML = title
+
+        result = []
+
+        result = data.dezenas
+
+        result.forEach((item) => {   
+
+            const list = document.getElementById("dezenas")
+
+            const listItem = `<li class="dezena">${item}</li>`
+
+            list.innerHTML += listItem
+
+        })
+    })
+    
+}
+
+function supersete() {
+
+    function clearUl() {
+        let ulDezenas = document.getElementById("dezenas");
+        ulDezenas.innerText = ""
+    }
+    
+    clearUl()
+
+    const url = `https://loteriascaixa-api.herokuapp.com/api/super-sete/latest`
+    fetch(url)
+    .then(response => response.json())
+    .then(data => {
+
+        const concurso = document.getElementById('concurso')
+
+        const lottery = data.nome
+        
+        const sorteio = data.concurso
+        
+        const date = data.data
+
+        const title = `Resultado da ${lottery} concurso ${sorteio} (${date})`
+
+        concurso.innerHTML = title
+
+        result = []
+
+        result = data.dezenas
+
+        result.forEach((item) => {   
+
+            const list = document.getElementById("dezenas")
+
+            const listItem = `<li class="dezena">${item}</li>`
+
+            list.innerHTML += listItem
+
+        })
+    })
+    
+}
